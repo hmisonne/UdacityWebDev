97bdc93c90fb6a8dba99976aaa87db7f66cf7968
hmisonne <helene.misonne@gmail.com>
2020-03-04 10:15:45 -0800

    Implemented request.get_json to avoid refreshing page to show new todo

--- ToDoApp/app.py
+++ ToDoApp/app.py
@@ -1,19 +1,56 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
 from flask_sqlalchemy import SQLAlchemy
+import sys
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:noisette@localhost:5432/postgres'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:noisette@localhost:5432/todoapp'
 db = SQLAlchemy(app)
 
 
 
-class Person(db.Model):
-  __tablename__ = 'persons'
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(), nullable=False)
+class Todo(db.Model):
+	__tablename__ = 'todos'
+	id = db.Column(db.Integer, primary_key=True)
+	description = db.Column(db.String(), nullable=False)
+	def __repr__(self):
+		return f'<Todo {self.id} {self.description}>'
+
 db.create_all()
 
 @app.route('/')
 def index():
-	person = Person.query.first()
-	return 'Hello ' + person.name
+	data = Todo.query.all()
+	return render_template('index.html', data=data)
+
+# @app.route('/todos/create', methods=['POST'])
+# def create_todo():
+#   description = request.get_json()['description']
+#   todo = Todo(description=description)
+#   db.session.add(todo)
+#   db.session.commit()
+#   return jsonify({
+#   		'description': todo.description
+#   	})
+
+@app.route('/todos/create', methods=['POST'])
+def create_todo():
+	error = False
+	body = {}
+	try:
+		description = request.get_json()['description']
+		todo = Todo(description=description)
+		db.session.add(todo)
+		db.session.commit()
+		body['description'] = todo.description
+	except:
+		db.session.rollback()
+		print(sys.exc_info())
+		error = True
+	finally:
+		db.session.close()
+	if error:
+		abort (400)
+	else:
+		return jsonify(body)
+
+		
