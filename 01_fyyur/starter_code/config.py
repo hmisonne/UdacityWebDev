4d38673ef1a729146e5b5be2df73faf0f783577c
hmisonne <helene.misonne@gmail.com>
2020-03-06 15:19:33 -0800

    implementation database and models on FyyurApp

---
+++ 01_fyyur/starter_code/config.py
@@ -0,0 +1,17 @@
+import os
+SECRET_KEY = os.urandom(32)
+# Grabs the folder where the script runs.
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# Enable debug mode.
+DEBUG = True
+
+# Connect to the database
+
+
+# TODO IMPLEMENT DATABASE URL
+# SQLALCHEMY_DATABASE_URI = '<Put your local database url>'
+SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:noisette@localhost:5432/fyyurapp'
+
+
+# Command Line: psql -U postgres fyyurapp
